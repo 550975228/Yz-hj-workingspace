40cc26b4fc1ee4e35f0a5d0b3e0300b8ed3c60bf
disguise <550975228@qq.com>
2020-08-05 18:15:11 +0800

    加了点文件说明，对skybox进行一定封装，等待集成到光照中去

--- README.md
+++ README.md
@@ -3,3 +3,60 @@
 这个工作空间是用来提交试用期的学习工作成果。
 
 #################################################################
+
+文件目录
+
+—lightingdemo	光照demo
+
+​	—libs	资源目录
+
+​		—images	图片放置位置
+
+​		—layui	layui插件，里面不展开，全是官网直接打包下载
+
+​		—cuon_matrix.js	webgl一些矩阵运算
+
+​		—cuon_utils.js	webgl一些基本工具，创建着色器程序，获取上下文等
+
+​		—sunCaculateFunction.js	关于太阳方向（主要是高度角和方位角）的计算方法
+
+​		—sunRealHour.js	真太阳时一年的时差（用于上面计算）
+
+​		—webgl_debug.js	帮助调试webgl的工具
+
+​		—webgl_utils.js	webgl基础工具箱
+
+​	—testplace
+
+​		—shadowMap.html	阴影贴图的demo
+
+​		—shadowMap.js	阴影贴图的js
+
+​		—skyBox.html	天空盒demo
+
+​		—skyBox.js	天空盒js
+
+​		—texture.html	纹理贴图的demo
+
+​		—texture.js 	问题贴图的js
+
+​		—z_slidertest.html	layui控件测试滑块的demo
+
+​		—z_slidertest.js	layui控件测试滑块的js
+
+​	—readme.md	一些进度和想法记录
+
+​	—sunlightlmitate.html	模拟一天的太阳光照的demo
+
+​	—sunlightlmitate.js	模拟一天的太阳光照的js
+
+—readme 	介绍
+
+—test.txt 	可能放些数据
+
+
+
+有新添文件会继续编辑
+
+
+
--- lightingdemo/testplace/skyBox.js
+++ lightingdemo/testplace/skyBox.js
@@ -1,135 +1,11 @@
 {
     var gl, fsForSkyBox, vsForSkyBox, canvas, skyboxProgram;
-    var then = 0;
-    var positionBuffer;
+    var skyPositionBuffer;
 }
-{
-    //注册鼠标事件
-    var mouseDown = false;
-    var lastMouseX = null;
-    var lastMouseY = null;
-    var angle_x = 0.0;
-    var angle_y = 0.0;
-    var fov = 1.5;
-    var aspect = 1.0;
-}
-var keys = {};
-
-function handleKeyDown(event)
-{
-    keys[event.keyCode] = true;
-}
-
-function handleKeyUp(event)
-{
-    keys[event.keyCode] = false;
-}
-function handleMouseDown(event) {
-    mouseDown = true;
-    lastMouseX = event.clientX;
-    lastMouseY = event.clientY;
-}
-
-function handleMouseUp(event) {
-    mouseDown = false;
-}
-
-function handleMouseMove(event) {
-    if (!mouseDown) {
-        return;
-    }
-    var newX = event.clientX;
-    var newY = event.clientY;
-
-    angle_x += (newY - lastMouseY) * 0.002;
-    angle_y -= (newX - lastMouseX) * 0.002;
-
-    angle_x = Math.max(Math.min(angle_x, Math.PI / 2), -Math.PI / 2);
-
-    lastMouseX = newX
-    lastMouseY = newY;
-
-    event.preventDefault();
-    requestAnimationFrame(draw);
-}
-
-function handleMouseWheel(event) {
-    var delta = 0.0;
-    if (event.deltaMode == 0)
-        delta = event.deltaY * 0.001;
-    else if (event.deltaMode == 1)
-        delta = event.deltaY * 0.03;
-    else
-        delta = event.deltaY;
-
-    fov *= Math.exp(-delta);
-    fov = Math.max(Math.min(fov, 2.5), 0.1);
-
-    event.preventDefault();
-    requestAnimationFrame(draw);
-}
-
-function rotateXY(angle_x, angle_y) {
-    var cosX = Math.cos(angle_x);
-    var sinX = Math.sin(angle_x);
-    var cosY = Math.cos(angle_y);
-    var sinY = Math.sin(angle_y);
-
-    return [
-        cosY, 0, -sinY, 0,
-        -sinX * sinY, cosX, -sinX * cosY, 0,
-        cosX * sinY, sinX, cosX * cosY, 0,
-        0, 0, 0, 1
-    ];
-}
-
-function perspectiveMatrixInverse(fov, aspect, near, far) {
-    var h = Math.tan(0.5 * fov);
-    var w = h * aspect;
-    var z0 = (far - near) / (-2 * far * near);
-    var z1 = (far + near) / (-2 * far * near);
-
-    return [
-        w, 0, 0, 0,
-        0, h, 0, 0,
-        0, 0, 0, z0,
-        0, 0, 1, z1
-    ];
-}
-
-//矩阵乘法a*b
-function mul(a, b) {
-    var r = [];
-
-    r[0] = a[0] * b[0] + a[1] * b[4] + a[2] * b[8] + a[3] * b[12];
-    r[1] = a[0] * b[1] + a[1] * b[5] + a[2] * b[9] + a[3] * b[13];
-    r[2] = a[0] * b[2] + a[1] * b[6] + a[2] * b[10] + a[3] * b[14];
-    r[3] = a[0] * b[3] + a[1] * b[7] + a[2] * b[11] + a[3] * b[15];
-
-    r[4] = a[4] * b[0] + a[5] * b[4] + a[6] * b[8] + a[7] * b[12];
-    r[5] = a[4] * b[1] + a[5] * b[5] + a[6] * b[9] + a[7] * b[13];
-    r[6] = a[4] * b[2] + a[5] * b[6] + a[6] * b[10] + a[7] * b[14];
-    r[7] = a[4] * b[3] + a[5] * b[7] + a[6] * b[11] + a[7] * b[15];
-
-    r[8] = a[8] * b[0] + a[9] * b[4] + a[10] * b[8] + a[11] * b[12];
-    r[9] = a[8] * b[1] + a[9] * b[5] + a[10] * b[9] + a[11] * b[13];
-    r[10] = a[8] * b[2] + a[9] * b[6] + a[10] * b[10] + a[11] * b[14];
-    r[11] = a[8] * b[3] + a[9] * b[7] + a[10] * b[11] + a[11] * b[15];
-
-    r[12] = a[12] * b[0] + a[13] * b[4] + a[14] * b[8] + a[15] * b[12];
-    r[13] = a[12] * b[1] + a[13] * b[5] + a[14] * b[9] + a[15] * b[13];
-    r[14] = a[12] * b[2] + a[13] * b[6] + a[14] * b[10] + a[15] * b[14];
-    r[15] = a[12] * b[3] + a[13] * b[7] + a[14] * b[11] + a[15] * b[15];
-
-    return r;
-}
-
 
 function main() {
     initProgramAndShader();
-    positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    setGeometry();
+    setSkyPosition();
     loadSkyBox();
     draw();
 
@@ -145,14 +21,15 @@ function initProgramAndShader() {
     fsForSkyBox = document.getElementById("fsForSkyBox").innerHTML;
     vsForSkyBox = document.getElementById("vsForSkyBox").innerHTML;
     skyboxProgram = createProgram(gl, vsForSkyBox, fsForSkyBox);
-    if (!skyboxProgram) console.log("初始化着色器失败");
+    if (!skyboxProgram) console.log("初始化天空盒着色器失败");
     //获取变量
     skyboxProgram.a_position = gl.getAttribLocation(skyboxProgram, "a_position");
-    skyboxProgram.u_viewDirectionProjectionInverse = gl.getUniformLocation(skyboxProgram, "u_viewDirectionProjectionInverse");
+    skyboxProgram.u_vpMatrix = gl.getUniformLocation(skyboxProgram, "u_vpMatrix");
     skyboxProgram.u_skybox = gl.getUniformLocation(skyboxProgram, "u_skybox");
-    if (skyboxProgram.a_position<0 || !skyboxProgram.u_viewDirectionProjectionInverse || !skyboxProgram.u_skybox) console.log("获取数据失败");
+    if (skyboxProgram.a_position < 0 || !skyboxProgram.u_vpMatrix || !skyboxProgram.u_skybox) console.log("获取数据失败");
 
 }
+
 function loadSkyBox() {
     var skyTexture = gl.createTexture();
     gl.activeTexture(gl.TEXTURE1);
@@ -210,7 +87,8 @@ function loadSkyBox() {
     gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
     gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
 }
-function setGeometry() {
+
+function setSkyPosition() {
     var positions = new Float32Array(
         [
             -1, -1,
@@ -220,60 +98,51 @@ function setGeometry() {
             1, -1,
             1, 1,
         ]);
+    skyPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, skyPositionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 }
-function draw(time) {
-    document.onkeydown = handleKeyDown;
-    document.onkeyup = handleKeyUp;
-    canvas.onmousedown = handleMouseDown;
-    document.onmouseup = handleMouseUp;
-    document.onmousemove = handleMouseMove;
-    canvas.onwheel = handleMouseWheel;
-    time *= 0.001;
-    var deltaTime = time - then;
-    then = time;
 
+function draw() {
+    drawSkyBox();
+    requestAnimationFrame(draw);
+}
+function drawSkyBox() {
     gl.viewport(0, 0, canvas.width, canvas.height);
-
     gl.enable(gl.CULL_FACE);
     gl.enable(gl.DEPTH_TEST);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     gl.enableVertexAttribArray(skyboxProgram.a_position);
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, skyPositionBuffer);
     var size = 2;
     var type = gl.FLOAT;
     var normalize = false;
     var stride = 0;
     var offset = 0;
-    gl.vertexAttribPointer(
-        skyboxProgram.a_position, size, type, normalize, stride, offset
-    );
-    var aspect =canvas.clientWidth / canvas.clientHeight;
-    var projectionMatrix = new Matrix4();
-    projectionMatrix.setPerspective(50, aspect, 1, 2000);
-
-    var cameraPosition = [Math.cos(then * .1), 0, Math.sin(then * .1)];
-    // console.log(Math.sin(time * .1));
-
-    var target = [0, 0, 0];
-    var up = [0, 1, 0];
+    gl.vertexAttribPointer(skyboxProgram.a_position, size, type, normalize, stride, offset);
+    setSkyVpMatrix();
+    gl.useProgram(skyboxProgram);
+    gl.uniform1i(skyboxProgram.u_skybox, 1);
+    gl.depthFunc(gl.LEQUAL);
+    gl.drawArrays(gl.TRIANGLES, 0, 1 * 6);
+    gl.useProgram(null);
+    gl.bindBuffer(gl.ARRAY_BUFFER,null);
+}
+function setSkyVpMatrix() {
     var cameraMatrix = new Matrix4();
-    // cameraMatrix.lookAt(Math.cos(time * .1), 0,  Math.sin(time * .1), 0, 0, 0, 0, 1, 0);
-    cameraMatrix.lookAt(0.9,0.0,0.1,0.0,0.0,0.0,0.0,1.0,0.0);
+    cameraMatrix.lookAt(0, 0, 0.1, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     var viewMatrix = new Matrix4();
     viewMatrix.setInverseOf(cameraMatrix);
-    // viewMatrix[12] = 0;
-    // viewMatrix[13] = 0;
-    // viewMatrix[14] = 0;
-    var viewDirectionProjectionMatrix = new Matrix4();
-    viewDirectionProjectionMatrix.multiply(projectionMatrix).multiply(viewMatrix);
-    var viewDirectionProjectionInverseMatrix = new Matrix4();
-    viewDirectionProjectionInverseMatrix.setInverseOf(viewDirectionProjectionMatrix);
+
+    var projectionMatrix = new Matrix4();
+    var aspect = canvas.clientWidth / canvas.clientHeight;
+    projectionMatrix.setPerspective(50, aspect, 1, 2000);
+
+    var vpMatrix = new Matrix4();
+    vpMatrix.multiply(projectionMatrix).multiply(viewMatrix);
+    vpMatrix.setInverseOf(vpMatrix);
     gl.useProgram(skyboxProgram);
-    gl.uniformMatrix4fv(skyboxProgram.u_viewDirectionProjectionInverse,false,viewDirectionProjectionInverseMatrix.elements);
-    gl.uniform1i(skyboxProgram.u_skybox,1);
-    gl.depthFunc(gl.LEQUAL);
-    gl.drawArrays(gl.TRIANGLES,0,1 * 6);
-    requestAnimationFrame(draw);
+    gl.uniformMatrix4fv(skyboxProgram.u_vpMatrix, false, vpMatrix.elements);
 }
+
+
